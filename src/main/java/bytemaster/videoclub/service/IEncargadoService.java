55d7064356ed5058f0e7dde562e222e9f24e44b8
x9laaa <acarreno95@outlook.com>
2023-08-29 22:18:03 -0400

    }

--- src/main/java/bytemaster/videoclub/service/IEncargadoService.java
+++ src/main/java/bytemaster/videoclub/service/IEncargadoService.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public interface IEncargadoService {
     public Encargado crearEncargdo(Encargado nuevoEncargdo);
+
     public Encargado actualizarEncargdo(int id);
+
     public List<Encargado> Encargdo();
+
     public Encargado listarEncargdoID(int idEncargdo);
+
     public void eliminarEncargdo(int id);
+}
