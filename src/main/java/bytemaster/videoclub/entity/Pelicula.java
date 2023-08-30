9553961f6082d8daaa73b61c86e8bed688f9420f
osvaldotoromontecinos@hotmail.com <osvaldotoromontecinos@hotmail.com>
2023-08-29 21:24:41 -0400

    Clase pelicula

--- src/main/java/bytemaster/videoclub/entity/Pelicula.java
+++ src/main/java/bytemaster/videoclub/entity/Pelicula.java
@@ -1,4 +1,28 @@
 package bytemaster.videoclub.entity;
-
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pelicula")
 public class Pelicula {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 75)
+    private String titulo;
+
+    @Column(length = 75)
+    private String genero;
+
+    @Column(length = 15)
+    private String Disponibilidad; //(Texto, por ejemplo: "Disponible", "Alquilada", "Reservada")
+
+    @Column(length = 10)
+    private double PrecioArriendo; //(Decimal)
+
 }
