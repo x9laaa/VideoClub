52235e6934bf168c424e6092814c648a3f1c1b9f
osvaldotoromontecinos@hotmail.com <osvaldotoromontecinos@hotmail.com>
2023-08-29 22:23:23 -0400

    IPeliculaService

---
+++ src/main/java/bytemaster/videoclub/service/IPeliculaService.java
@@ -0,0 +1,11 @@
+package bytemaster.videoclub.service;
+import bytemaster.videoclub.entity.Pelicula;
+import java.util.List;
+
+public interface IPeliculaService {
+        public Pelicula crearPelicula(Pelicula nuevaPelicula);
+        public Pelicula actualizarPelicula(int id);
+        public List<Pelicula> listarPelicula();
+        public Pelicula listarPeliculasID(int idPelicula);
+        public void eliminarPelicula(int id);
+}
