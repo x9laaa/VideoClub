68604607d91df2a3d4b1314675e169559725b3e6
pmondaca <pmondaca@gmail.com>
2023-08-29 21:22:15 -0400

    ClASE ARRIENDO

--- src/main/java/bytemaster/videoclub/entity/Arriendo.java
+++ src/main/java/bytemaster/videoclub/entity/Arriendo.java
@@ -1,4 +1,39 @@
 package bytemaster.videoclub.entity;
 
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "arriendo")
+
+
 public class Arriendo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "ListaPeliculas");
+    private List<Pelicula> peliculas;
+
+    private Encargado encargadoAsiganado;
+
+    private LocalDate fecha;
+
+    @Column(length = 30)
+    private int precioTotal;
+
+
+
+
 }
