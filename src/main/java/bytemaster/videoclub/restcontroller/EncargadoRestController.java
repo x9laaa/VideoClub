64a5c6d864de790ed532691dc2dc40c240365501
x9laaa <acarreno95@outlook.com>
2023-08-31 20:22:53 -0400

    Pelicula rest controller

--- src/main/java/bytemaster/videoclub/restcontroller/EncargadoRestController.java
+++ src/main/java/bytemaster/videoclub/restcontroller/EncargadoRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Encargado")
+@RequestMapping("/api/encargado")
 public class EncargadoRestController {
     @Autowired
     IEncargadoService objEncargadoService;
--- src/main/java/bytemaster/videoclub/restcontroller/PeliculaRestController.java
+++ src/main/java/bytemaster/videoclub/restcontroller/PeliculaRestController.java
@@ -1,4 +1,39 @@
 package bytemaster.videoclub.restcontroller;
 
+import bytemaster.videoclub.entity.Pelicula;
+import bytemaster.videoclub.service.IPeliculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pelicula")
 public class PeliculaRestController {
+    @Autowired
+    IPeliculaService objPeliculaService;
+
+    @PostMapping
+    public Pelicula crearPelicula(@RequestBody Pelicula nuevoPelicula){
+        return objPeliculaService.crearPelicula(nuevoPelicula);
+    }
+    @PostMapping
+    public Pelicula actualizarPelicula(@RequestBody int id){
+        return  objPeliculaService.actualizarPelicula(id);
+    }
+
+    @GetMapping
+    public List<Pelicula> listarPelicula(){
+        return objPeliculaService.listarPelicula();
+    }
+
+    @GetMapping
+    public Pelicula listarPeliculaID(int idPelicula){
+        return  objPeliculaService.listarPeliculasID(idPelicula);
+    }
+
+    @PostMapping
+    public void eliminarPelicula(@RequestBody int id){
+        objPeliculaService.eliminarPelicula(id);
+    }
 }
