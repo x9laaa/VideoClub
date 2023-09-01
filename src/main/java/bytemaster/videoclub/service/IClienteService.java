6848559d8262dabd2d2e9733a3a821f3cbc8624b
fvmolina21 <fvmolina@uc.cl>
2023-08-31 21:11:48 -0400

    Cliente_imp

--- src/main/java/bytemaster/videoclub/service/IClienteService.java
+++ src/main/java/bytemaster/videoclub/service/IClienteService.java
@@ -7,11 +7,13 @@ import java.util.List;
 public interface IClienteService {
     public Cliente crearCliente(Cliente nuevoCliente);
 
-    public Cliente actualizarCliente(int id);
+    public Cliente actualizarCliente(int id, Cliente clienteactualizar);
 
     public List<Cliente> listarCliente();
 
     public Cliente listarClienteID(int idCliente);
 
     public void eliminarCliente(int id);
+
+    public void eliminarCliente2(Cliente cliente);
 }
