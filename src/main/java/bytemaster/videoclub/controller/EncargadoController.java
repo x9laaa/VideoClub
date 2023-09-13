b853842ce58e7a7db3b7dcf807b7d403bddb8818
x9laaa <acarreno95@outlook.com>
2023-09-13 10:39:50 -0300

    funcion de editar en vista y controller de encargado

--- src/main/java/bytemaster/videoclub/controller/EncargadoController.java
+++ src/main/java/bytemaster/videoclub/controller/EncargadoController.java
@@ -5,10 +5,7 @@ import bytemaster.videoclub.service.IEncargadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,18 @@ public class EncargadoController {
     @PostMapping("/eliminar/{id}")
     public String eliminarEncargado(@PathVariable int id){
         objEncargadoService.eliminarEncargado(id);
+        return "redirect:/encargado";
+    }
+    @GetMapping("/editar/{id}")
+    public String formularioEditarEncargado(@PathVariable int id, Model model){
+        Encargado encargado = objEncargadoService.listarEncargadoID(id);
+        model.addAttribute("attributeEncargado",encargado);
+        return "editarEncargado";
+    }
+
+    @PostMapping("/editar")
+    public String editarEncargado(@ModelAttribute Encargado encargado){
+        objEncargadoService.actualizarEncargado(encargado.getId(),encargado);
         return "redirect:/encargado";
     }
 }
